d32a1b322d1ff5bf04402e90b9958d4683569f72
isrart <isrart.developer@gmail.com>
2019-02-23 22:54:38 +0100

    integración firebase

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,59 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import firebase from 'firebase';
+import AuthGuard from '@/guards/auth.guard';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '*',
+      redirect: '404',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/Login.vue'),
+      beforeEnter: (to, from, next) => {
+        const usuario = firebase.auth().currentUser;
+        if (usuario) {
+          next('/');
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: () => import('./views/404.vue'),
+    },
+    {
+      path: '/',
+      component: () => import('./views/LayoutPrincipal.vue'),
+      meta: {
+        autorizado: true,
+      },
+      children: [
+        {
+          path: '/about',
+          name: 'about',
+          component: () => import('./views/About.vue'),
+        },
+        {
+          path: '',
+          name: 'home',
+          component: () => import('./views/Home.vue'),
+        },
+      ],
     },
   ],
 });
+
+/* GUARDS */
+new AuthGuard(router).generateGuard();
+
+export default router;
---
+++ src/views/LayoutPrincipal.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <navbar />
+    <router-view/>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Navbar from '@/components/Navbar.vue';
+
+export default Vue.extend({
+  components: {
+    Navbar,
+  },
+});
+</script>
